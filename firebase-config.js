2dd13a7630acb7a048a43e0798a81d0d959d1aad
RafaeeeelCom4E <19pablorafael@gmail.com>
2026-07-30 22:31:19 -0300

    atualizacao 3.1

--- firebase-config.js
+++ firebase-config.js
@@ -32,10 +32,10 @@
 // ============================================================
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJETO",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyBPNtZ1GjhK29JoLK7IU4DoKHo7bKBXtss",
+  authDomain: "teupaiestudos.firebaseapp.com",
+  projectId: "teupaiestudos",
+  storageBucket: "teupaiestudos.firebasestorage.app",
+  messagingSenderId: "501378994524",
+  appId: "1:501378994524:web:fd493529d5e4a2f83af199",
 };
